b45c0ef4d175d099911245c7152f71913d2ea982
prashant-bharaj <prashant.bharaj@gmail.com>
2025-12-26 13:49:12 +0100

    chore: update package versions and add meta dependency
    - Updated versions for several packages in pubspec.lock.
    - Added 'meta' package import in image_save_utils.dart, raw_editor_state_text_input_client_mixin.dart, and link.dart files.

--- flutter_quill_extensions/lib/src/editor/image/image_save_utils.dart
+++ flutter_quill_extensions/lib/src/editor/image/image_save_utils.dart
@@ -4,6 +4,7 @@ library;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:meta/meta.dart';
 import 'package:flutter_quill/internal.dart';
 import 'package:path/path.dart' as p;
 
